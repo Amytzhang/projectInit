99d240d48c1dc0f8877e4bb6417ccea9dad6a36c
e <2429323352@qq.com>
2020-11-20 09:26:04 +0800

    commit to master

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,27 @@
 const path = require('path')
-const HtmlWebpackPlugin = require()
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-  // mode: 'development',
+  mode: 'development',
   entry: "./src/index.js",
-  publicPath: process.env.NODE_ENV === 'production'?'/app/':'/',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname,'dist')
   },
+  devServer:{
+    port: 8080,
+    open: true,
+    compress: true
+  },
   plugins:[
     new HtmlWebpackPlugin({
-      template: './index.html',
-      hash: true
+      template: './src/test.html',
+      hash: true,
+      minify:{
+        removeAttributeQuotes: true,
+        collapseInlineTagWhitespace: true,
+        removeComments:true
+      },
+      filename: 'ab.html'
     })
   ]
 }
